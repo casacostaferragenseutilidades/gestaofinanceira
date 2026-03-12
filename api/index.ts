13ffd3680731db6be45b179c364a6e51b9baaa3c
Casa Costa Ferragens <casacostaferragenseutilidades@github.com>
2026-03-12 18:13:01 -0300

    debug: internal import with .js extension

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { testValue } from "./test";
+import { testValue } from "./test.js";
 
 export default function(req: any, res: any) {
   res.status(200).json({ testValue, time: new Date().toISOString() });
